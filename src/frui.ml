6a985914df254b31c95d5153ab7aabf6cd6bc78d
Henry Hughes <henryrhughes@gmail.com>
2010-12-28 19:40:43 +0000

    detect when mouse is pressed down

--- src/frui.ml
+++ src/frui.ml
@@ -9,54 +9,71 @@ object
   method log (msg : string) =  ignore(<:exp< console.log($msg$) >>)
 end
 
+let set_text (e : D.element) text =
+  let text_node = (D.document#createTextNode "" : D.text) in
+  text_node#_set_data text;
+  ignore (e#appendChild text_node)
+
+let mouse_down_e () =
+  let e, s = F.make_event () in
+  let f me = F.send s true in
+  Dom.document#addEventListener_mouseEvent_ "mousedown" f false;
+  F.cleanup (fun () -> Dom.document#removeEventListener_mouseEvent_ "mousedown" f false);
+  e
+
 class dialog (elt : D.element) =
 object (self)
   val dialog = (D.document#createElement "div")
+  val mutable moving = false
+  
   method add_title =
     let title = (D.document#createElement "div") in
-    let style = title#_get_style in
-    ignore (style#_set_border "1px solid black");
     let close = (D.document#createElement "div") in
-    let title_text = (D.document#createTextNode "" : D.text) in
-    title_text#_set_data "title";
-    ignore (title#appendChild title_text);
-    let close_text = (D.document#createTextNode "" : D.text) in
-    close_text#_set_data "x";
-    let close_style = close#_get_style in
-    ignore (close_style#_set_display "inline-block");
-    ignore (close_style#_set_position "absolute");
-    ignore (close_style#_set_right "5px");
-    ignore (close_style#_set_cursor "hand");
+    set_text title "title";
+    set_text close "x";
+    ignore (title#_get_style#_set_border "1px solid black");
+    ignore (close#_get_style#_set_display "inline-block");
+    ignore (close#_get_style#_set_position "absolute");
+    ignore (close#_get_style#_set_right "5px");
+    ignore (close#_get_style#_set_cursor "hand");
     close#_set_onclick
       (fun _ -> ignore (elt#removeChild dialog); true);
-    ignore (close#appendChild close_text);
     ignore (title#appendChild close);
     ignore (dialog#appendChild title)
 
   method add_body =
     let body = (D.document#createElement "div") in
-    let style = body#_get_style in
-    ignore (style#_set_border "1px solid black");
-    ignore (style#_set_borderTop "1px none black");
-    ignore (style#_set_height "150px");
-    let text = (D.document#createTextNode "" : D.text) in
-    text#_set_data "body";
-    ignore (body#appendChild text);
+    set_text body "body";
+    ignore (body#_get_style#_set_border "1px solid black");
+    ignore (body#_get_style#_set_borderTop "1px none black");
+    ignore (body#_get_style#_set_height "150px");
     ignore (dialog#appendChild body)
+
+  method add_mouse_events =
+    let mouse_move = Fd.mouse_b () in
+    Fd.appendChild elt
+    (F.blift mouse_move (fun (x,y) ->
+      if not moving then dialog
+      else
+	begin
+	  dialog#_get_style#_set_left (string_of_int x);
+	  dialog#_get_style#_set_top (string_of_int y);
+	  dialog
+	end));
+    let mouse_down = F.hold (false) (mouse_down_e ()) in
+    let nb = F.blift mouse_down (fun b -> b) in
+    F.notify_result_b nb (fun r -> match r with
+      |  F.Value b -> (moving <- b)
+      | _ -> ())
+
 
   method decorate =
     self#add_title;
     self#add_body;
-    let style = dialog#_get_style in
-    ignore (style#_set_display "inline-block");
-    ignore (style#_set_position "absolute");
-    ignore (style#_set_width "200px");
-    let mouse = Fd.mouse_b () in
-    Fd.appendChild elt
-    (F.blift mouse (fun (x,y) ->
-      dialog#_get_style#_set_left (string_of_int x);
-      dialog#_get_style#_set_top (string_of_int y);
-      dialog))
+    self#add_mouse_events;
+    ignore (dialog#_get_style#_set_display "inline-block");
+    ignore (dialog#_get_style#_set_position "absolute");
+    ignore (dialog#_get_style#_set_width "200px");
 
   initializer self#decorate
 end
