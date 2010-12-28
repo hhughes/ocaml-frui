61563ad1246eeb85568ade52d3bcf1a6835ba0ee
Henry Hughes <henryrhughes@gmail.com>
2010-12-28 21:36:18 +0000

    only put filter on when mouse presses are detected

--- src/frui.ml
+++ src/frui.ml
@@ -16,14 +16,16 @@ let set_text (e : D.element) text =
   text_node#_set_data text;
   ignore (e#appendChild text_node)
 
-let mouse_down_e (elt : D.element) =
+let mouse_down_if_e p (elt : D.element) =
   let e, s = F.make_event () in
-  let f_down me = F.send s true in
-  let f_up me = F.send s false in
+  let f_down me = if p me then F.send s true in
+  let f_up me = if p me then F.send s false in
   elt#addEventListener_mouseEvent_ "mousedown" f_down false;
   elt#addEventListener_mouseEvent_ "mouseup" f_up false;
   F.cleanup (fun () -> elt#removeEventListener_mouseEvent_ "mousedown" f_down false; elt#removeEventListener_mouseEvent_ "mouseup" f_up false);
   e
+
+let mouse_down_e (elt : D.element) = mouse_down_if_e (fun x -> true) elt
 
 class dialog (elt : D.element) =
 object (self)
@@ -64,8 +66,23 @@ object (self)
 	  dialog#_get_style#_set_top (string_of_int y);
 	  dialog
 	end));
-    let mouse_down = F.hold (false) (mouse_down_e dialog) in
-    F.notify_result_b (F.blift mouse_down (fun b -> b)) (fun r -> match r with
+    let is_resize me =
+      let x = me#_get_clientX in
+      let y = me#_get_clientY in
+      let l = dialog#_get_offsetLeft in
+      let t = dialog#_get_offsetTop in
+      let w = dialog#_get_offsetWidth in
+      let h = dialog#_get_offsetHeight in
+      debug (string_of_int x);
+      debug (string_of_int y);
+      debug (string_of_int l);
+      debug (string_of_int t);
+      debug (string_of_int w);
+      debug (string_of_int h);
+      x > (l+w)-5 && y > (t+h)-5
+    in
+    let mouse_down_move = F.hold (false) (mouse_down_if_e (fun me -> is_resize me) dialog) in
+    F.notify_result_b (F.blift mouse_down_move (fun b -> b)) (fun r -> match r with
       |  F.Value b -> (moving <- b)
       | _ -> ())
 
