4009a36d9c24a95adedfaad9e1602d21835f6aed
Henry Hughes <henryrhughes@gmail.com>
2011-01-20 16:23:52 +0000

    thread bars should have relative lengths

--- src/visualiser/thread.ml
+++ src/visualiser/thread.ml
@@ -1,4 +1,5 @@
 open Msg
+open Logger
 
 class thread i =
 object
@@ -6,7 +7,14 @@ object
   val mutable id = -1
   val mutable start = -1 (* -1 means the thread start time is unknown *)
   val mutable finish = -1 (* -1 means the thread is yet to finish *)
-  method set_msgs m = msgs <- m
+  method parse_msg msg = 
+    (match Msg.ty msg with
+      | "t_start" -> start <- (Msg.timestamp msg)
+      | "t_finish" -> finish <- (Msg.timestamp msg)
+      | _ -> ());
+	debug (string_of_int start);
+	debug (Msg.ty msg);
+    msgs <- msg :: msgs
   method msgs = msgs
   method start = start
   method finish = finish
--- src/visualiser/visualiser.ml
+++ src/visualiser/visualiser.ml
@@ -11,23 +11,25 @@ let add_to_thread msg =
   let tid = Msg.threadId msg in
   if not (Hashtbl.mem threads tid) then Hashtbl.add threads tid (new thread tid);
   let thread = Hashtbl.find threads tid in
-  thread#set_msgs (msg :: thread#msgs)
+  thread#parse_msg msg
 
-let render_msg (thread_elt : Dom.element) msg = 
+let render_msg (thread_elt : Dom.element) thread msg = 
   let msg_elt = (Dom.document#createElement "div" : Dom.element) in
   ignore (msg_elt#_set_className (Msg.ty msg));
   ignore (msg_elt#_set_title (Msg.desc msg));
-  ignore (msg_elt#_get_style#_set_left (string_of_int (2 * (Msg.timestamp msg))));
+  ignore (msg_elt#_get_style#_set_left (string_of_int (10 * ((Msg.timestamp msg) - thread#start))));
   ignore (thread_elt#appendChild msg_elt)
 
 let render_thread id thread =
   let thread_elt = (Dom.document#createElement "div" : Dom.element) in
   ignore (thread_elt#_set_className "thread");
   ignore (thread_elt#_get_style#_set_top (string_of_int (25 * id)));
+  ignore (thread_elt#_get_style#_set_left (string_of_int (10 * thread#start)));
+  ignore (thread_elt#_get_style#_set_width (string_of_int (10 * (thread#finish - thread#start))));
   (*let thread_text = (Dom.document#createTextNode (string_of_int id) : Dom.text) in
   ignore (thread_elt#appendChild thread_text);*)
   ignore (main_elt#appendChild thread_elt);
-  List.iter (fun m -> render_msg thread_elt m) thread#msgs
+  List.iter (render_msg thread_elt thread) thread#msgs
 
 let render_threads () = Hashtbl.iter render_thread threads
 
@@ -37,7 +39,9 @@ let load_objects o s =
       begin
 	let msgs = unmarshall_json o in
 	let add_msg msg = match Msg.ty msg with
-	  | "msg" -> add_to_thread msg
+	  | "msg"
+	  | "t_start"
+	  | "t_finish" -> add_to_thread msg
 	  | _ -> ()
        	in
 	for_each add_msg msgs;
