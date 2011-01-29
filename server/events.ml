f766e01bdee3c6eddc44131ab4e1db154b19d71c
Henry Hughes <henryrhughes@gmail.com>
2011-01-29 21:16:44 +0000

    more interesting "random" data

--- server/events.ml
+++ server/events.ml
@@ -3,17 +3,17 @@ open Json_type
 open Build
 open Json_io
 
-type msg_type = Msg | FunStart | FunEnd | TStart | TEnd
+type event_type = Msg | FunStart | FunEnd | TStart | TEnd
 
-type msg = {
+type event = {
   tid: int;
-  ty: msg_type;
+  ty: event_type;
   ts: int;
   name: string;
   desc: string;
 }
 
-let create_msg tid ty ts name desc = {
+let create_event tid ty ts name desc = {
   tid = tid;
   ty = ty;
   ts = ts;
@@ -21,22 +21,22 @@ let create_msg tid ty ts name desc = {
   desc = desc;
 }
 
-let string_of_msg_type = function
+let string_of_event_type = function
   | Msg -> "msg"
   | FunStart -> "fn_start"
   | FunEnd -> "fn_finish"
   | TStart -> "t_start"
   | TEnd -> "t_finish"
 
-let _jsonify msg = objekt [
-    ("tid", int msg.tid);
-    ("ty", string (string_of_msg_type msg.ty));
-    ("ts", int msg.ts);
-    ("name", string msg.name);
-    ("desc", string msg.desc);
+let _jsonify event = objekt [
+    ("tid", int event.tid);
+    ("ty", string (string_of_event_type event.ty));
+    ("ts", int event.ts);
+    ("name", string event.name);
+    ("desc", string event.desc);
     ("misc", objekt [])] 
 
-let jsonify msgs = array (List.map _jsonify msgs)
+let jsonify events = array (List.map _jsonify events)
 
 let count = ref 20
 
@@ -44,8 +44,12 @@ let next_count () =
   let c = !count in
   count := !count + 1; c
 
-(*let _create_fn_start () =*)
 
-let _create_msg () = jsonify [create_msg 0 Msg (next_count ()) "some name" "message"]
+let create_events () = [
+  create_event 0 Msg (next_count ()) "some name" "message";
+  create_event 2 TEnd (next_count ()) "t2" "t2";
+  create_event 2 Msg (next_count ()) "some name" "message";
+  create_event 2 TStart (next_count ()) "t2" "t2";
+]
 
-let create_msg () = string_of_json (_create_msg ())
+let create_msg () = string_of_json (jsonify (create_events ()))
