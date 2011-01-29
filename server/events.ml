072039a98e77e83899ecf48dd1bc12b3f949138f
Henry Hughes <henryrhughes@gmail.com>
2011-01-29 21:09:10 +0000

    lets use objects to create messages

--- server/events.ml
+++ server/events.ml
@@ -3,18 +3,49 @@ open Json_type
 open Build
 open Json_io
 
+type msg_type = Msg | FunStart | FunEnd | TStart | TEnd
+
+type msg = {
+  tid: int;
+  ty: msg_type;
+  ts: int;
+  name: string;
+  desc: string;
+}
+
+let create_msg tid ty ts name desc = {
+  tid = tid;
+  ty = ty;
+  ts = ts;
+  name = name;
+  desc = desc;
+}
+
+let string_of_msg_type = function
+  | Msg -> "msg"
+  | FunStart -> "fn_start"
+  | FunEnd -> "fn_finish"
+  | TStart -> "t_start"
+  | TEnd -> "t_finish"
+
+let _jsonify msg = objekt [
+    ("tid", int msg.tid);
+    ("ty", string (string_of_msg_type msg.ty));
+    ("ts", int msg.ts);
+    ("name", string msg.name);
+    ("desc", string msg.desc);
+    ("misc", objekt [])] 
+
+let jsonify msgs = array (List.map _jsonify msgs)
+
 let count = ref 20
 
 let next_count () =
   let c = !count in
   count := !count + 1; c
 
-let _create_msg () = objekt [
-  ("tid", int 0);
-  ("ty", array [string "msg"]);
-  ("ts", int (next_count ()));
-  ("name", string "some name");
-  ("desc", string "message1");
-  ("misc", objekt [])]
+(*let _create_fn_start () =*)
+
+let _create_msg () = jsonify [create_msg 0 Msg (next_count ()) "some name" "message"]
 
 let create_msg () = string_of_json (_create_msg ())
