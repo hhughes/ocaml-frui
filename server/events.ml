4ff823a47d2766685d3ed399d071baf9f840bf06
Henry Hughes <henryrhughes@gmail.com>
2011-01-29 20:16:47 +0000

    add json-wheel as dependency, use this to create json object

--- server/events.ml
+++ server/events.ml
@@ -1,4 +1,7 @@
 open Printf
+open Json_type
+open Build
+open Json_io
 
 let count = ref 20
 
@@ -6,14 +9,12 @@ let next_count () =
   let c = !count in
   count := !count + 1; c
 
-let _create_msg () = sprintf
-"{
-  tid: 0,
-  ty: ['msg'],
-  ts: %d,
-  name: 'some name',
-  desc: 'message1',
-  misc: {}
-}" (next_count ())
+let _create_msg () = objekt [
+  ("tid", int 0);
+  ("ty", array [string "msg"]);
+  ("ts", int (next_count ()));
+  ("name", string "some name");
+  ("desc", string "message1");
+  ("misc", objekt [])]
 
-let create_msg () = sprintf "[%s]" (_create_msg ())
+let create_msg () = string_of_json (_create_msg ())
